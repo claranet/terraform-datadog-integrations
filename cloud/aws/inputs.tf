5b3d17a36d06c2b6bcb740d16fde987f003856a7
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2018-05-16 19:34:22 +0200

    MON-205 use data resource for trust relationship policy

--- cloud/aws/inputs.tf
+++ cloud/aws/inputs.tf
@@ -1,3 +1,10 @@
-variable "aws_external_id" {
+variable "datadog_aws_external_id" {
   description = "Token provided by DataDog to connect with the AWS account"
+  type        = "string"
+}
+
+variable "datadog_aws_account_id" {
+  description = "AWS account_id of Datadog"
+  type        = "string"
+  default     = "464622532012"
 }
