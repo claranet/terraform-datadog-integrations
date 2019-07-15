1132f4b958fde343e6af78db25b82bcdf05bd45f
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2019-07-15 19:37:24 +0200

    MON-413 add description to variables

--- alerting/pagerduty/inputs.tf
+++ alerting/pagerduty/inputs.tf
@@ -1,11 +1,15 @@
 variable "subdomain" {
+  type = string
+  description = "Pagerduty subdomain for your organisation"
   default = "claranet"
 }
 
 variable "api_token" {
   type = string
+  description = "Pagerduty API token"
 }
 
 variable "services" {
   type = map(string)
+  description = "Pagerduty service name and service key couple"
 }
