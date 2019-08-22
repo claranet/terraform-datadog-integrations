e91caf138533272ce4d95c793490056177164534
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2019-08-22 21:54:41 +0200

    MON-494 import refactoring from monitors surset scripts

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function goto_root {
+function goto_root() {
     script_dir=$(dirname $0)
     if [[ "$script_dir" == "." ]]; then
         cd ..
@@ -9,14 +9,34 @@ function goto_root {
     fi
 }
 
-function get_scope {
-    TO_PARSE="."
-    if [ ! -z ${1+x} ]; then
+function init() {
+    set -euo pipefail
+    if [[ ${GITLAB_CI:-} == "true" ]]; then
+        set -x
+    fi
+    # MON-478 fix sort order behavior on case
+    export LC_COLLATE=C
+    goto_root
+    if ! [ -z ${1:-} ]; then
+        cd "$1"
+    fi
+}
+
+function get_scope() {
+    TO_PARSE="./"
+    if [ ! -z ${1+x} ] && [ $1 != "." ]; then
         TO_PARSE="$1"
+    fi
+    if [[ $TO_PARSE != ./* ]]; then
+        TO_PARSE="./${TO_PARSE}"
     fi
     echo $TO_PARSE
 }
 
-function list_dirs {
+function list_dirs() {
     echo ${1} | awk -F '/' '{$1=""; print $0}' | cut -c 2-
+}
+
+function browse_modules() {
+    find "$1" -name "$2" -exec dirname "{}" \; | sort -fdbiu
 }
