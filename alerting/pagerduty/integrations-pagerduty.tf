ae96c6b11821e95737c6c6cd03d17b04cbe6370c
Jérôme Respaut <jerome.respaut@claranet.com>
2025-06-25 14:35:31 +0200

    refactor: ♻️ cleanup dead code

--- alerting/pagerduty/integrations-pagerduty.tf
+++ alerting/pagerduty/integrations-pagerduty.tf
@@ -1,16 +1,4 @@
-# resource "datadog_integration_pagerduty" "pagerduty" {
-#   lifecycle {
-#     ignore_changes = [
-#       api_token
-#     ]
-#   }
-
-#   subdomain = var.subdomain
-#   api_token = var.api_token
-# }
-
 resource "datadog_integration_pagerduty_service_object" "service" {
-  #   depends_on   = [datadog_integration_pagerduty.pagerduty]
   for_each     = var.services
   service_name = each.key
   service_key  = each.value
